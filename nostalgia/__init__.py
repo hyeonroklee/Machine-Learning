1485078534db2d5bd8adf9ca1a665967073a1eec
HyeonRok Lee <sealab2012@gmail.com>
2015-07-01 11:37:00 +0900

    update

--- nostalgia/__init__.py
+++ nostalgia/__init__.py
@@ -7,16 +7,20 @@
 
 from . import classification
 from . import regression
+from . import network
 from . import features
 from . import genetic
 from . import sampling
 from . import simulator
+from . import boosting
 
 __all___ = [
     'classification',
     'regression',
+    'network',
     'features',
     'genetic',
     'sampling',
-    'simulator'
+    'simulator',
+    'boosting'
 ]
