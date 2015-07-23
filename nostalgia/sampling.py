d77f27d015b4d2c9144d341c1aee29e6f0eef73f
HyeonRok Lee <sealab2012@gmail.com>
2015-07-24 00:25:46 +0900

    update

--- nostalgia/sampling.py
+++ nostalgia/sampling.py
@@ -1,29 +1,36 @@
 # -*- coding: utf-8 -*-
-"""
-
-@author: hyeonrok lee
-
-"""
 
 import numpy as np
 
+def split_train_test(x,y=None,ratio=0.75):
+    split_size = len(x)*ratio
+    if y is not None:
+        if len(x) != len(y):
+            raise Exception('dimension unmatched')
+        return x[:split_size],x[split_size:],y[:split_size],y[split_size:]
+    else:
+        return x[:split_size],x[split_size:]
+
+def bootstrapping(x,y=None,w=None,ratio=1.):
+    sampling_size = int(len(x)*ratio)
+    x_sampling = []
+    y_sampling = []
+
+    if w is None:
+        w = np.cumsum(np.repeat(1./float(len(x)),len(x)))
+    c = np.cumsum(w)
+
+    for i in range(sampling_size):
+        idx = np.where(c>=np.random.uniform())[0][0]
+        x_sampling.append(x[idx])
+        y_sampling.append(y[idx])
+
+    return x_sampling,y_sampling
+
+
 def cross_validation(d,k):
     if len(d) < k or len(d) % k != 0:
-        raise Exception('dimention unmatched')
+        raise Exception('dimension unmatched')
     data = np.array(d)
     for i in range(len(d) / k):
         print data[i*k:i*k+k],data[:i*k],data[i*k+k:len(d)]
-
-def bootstrapping(d):
-    data = np.array(d)
-    return data[np.random.randint(0,len(d),len(d))]
-
-if __name__ == '__main__':
-    d = [ [1,2], [3,4], [5,6] ]
-    cross_validation(d,1)
-'''
-    try:
-        cross_validation(d,1)
-    except Exception as e:
-        print str(e)
-'''
