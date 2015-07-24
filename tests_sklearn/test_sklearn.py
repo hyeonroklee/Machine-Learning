6bbdcf91f0321274cf9a574d663e063b1dfed775
HyeonRok Lee <sealab2012@gmail.com>
2015-07-24 15:00:11 +0900

    update

--- tests_sklearn/test_sklearn.py
+++ tests_sklearn/test_sklearn.py
@@ -1,41 +1,36 @@
 import numpy as np
-import pandas as pd
-
 import matplotlib.pyplot as plt
 
 from sklearn.datasets import samples_generator
-from sklearn.linear_model import LinearRegression
-from sklearn.cross_validation import train_test_split
+from sklearn.cross_validation import train_test_split,cross_val_score
+from sklearn.linear_model import LinearRegression,LogisticRegression
+from sklearn.svm import SVC
 
 def test_linear_regression():
     x,y = samples_generator.make_regression(n_features=1,noise=10.)
-    print x
-    print y
+    x_train,x_test,y_train,y_test = train_test_split(x,y)
+    predictor = LinearRegression()
+    predictor.fit(x_train,y_train)
+    print predictor.score(x_test,y_test),predictor.intercept_,predictor.coef_
 
-# spam filter
-# def test_logistic_regression():
-#     df = pd.read_csv('SMSSpamCollection',delimiter='\t',header=None)
-#     X_train_raw,X_test_raw,y_train,y_test = train_test_split(df[1],df[0])
-#     vectorize = TfidfVectorizer()
-#     vectorize.fit(df[1])
-#     X_train = vectorize.transform(X_train_raw)
-#     X_test = vectorize.transform(X_test_raw)
-#     classifier = LogisticRegression()
-#     classifier.fit(X_train,y_train)
-#     predictions = classifier.predict(X_test)
-#
-#     print y_test
-#     print predictions
-#
-#     # print vectorize.vocabulary_
-#     # print vectorize.fit_transform(X_train_raw).todense()
-#     # print vectorize.vocabulary_
-#
-# def test_decision_tree():
-#     pass
-#
-# def test_perceptron():
-#     pass
+    cv = cross_val_score(LinearRegression(),x,y,cv=10)
+    print np.mean(cv),cv
+
+def test_logistic_regression():
+    x,y = samples_generator.make_classification(n_features=2,n_redundant=0)
+    x_train,x_test,y_train,y_test = train_test_split(x,y)
+    predictor = LogisticRegression()
+    predictor.fit(x_train,y_train)
+    print predictor.score(x_test,y_test),predictor.intercept_,predictor.coef_
+
+def test_svm():
+    x,y = samples_generator.make_classification(n_features=2,n_redundant=0)
+    x_train,x_test,y_train,y_test = train_test_split(x,y)
+    predictor = SVC()
+    predictor.fit(x_train,y_train)
+    print predictor.score(x_test,y_test)
 
 if __name__ == '__main__':
-    test_linear_regression()
+    # test_linear_regression()
+    # test_logistic_regression()
+    test_svm()
