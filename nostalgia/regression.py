04e4caf07aff25a912d84882df1db42a860534b7
HyeonRok Lee <sealab2012@gmail.com>
2016-03-17 23:38:18 +0900

    update

--- nostalgia/regression.py
+++ nostalgia/regression.py
@@ -29,24 +29,25 @@ class LinearRegression(Regression):
         self._w = None
         self._ne = ne
 
-    def train(self,x,y):
+    def train(self,x,y,alpha=0.001,batch_size=10):
         if self._ne:
             t = np.matrix(np.append(x,np.ones((len(x),1)),axis=1))
             y = np.matrix(y)
             self._w = np.linalg.inv(t.T * t) * (t.T * y)
         else:
-            t = np.append(x,np.ones((len(x),1)),axis=1)
-            w = np.random.normal(size=(t.shape[1],1))
-            learning_rate = 0.001
+            t = np.matrix(np.append(x,np.ones((len(x),1)),axis=1))
+            y = np.matrix(y)
+            w = np.matrix(np.random.normal(size=(t.shape[1],1)))
             while True:
-                delta = np.zeros(t.shape[1])
-                for i in range(len(t)):
-                    delta += (learning_rate * (y[i] - np.dot(t[i],w))) * t[i]
-                if np.all(np.abs(delta) < 0.001):
+                delta = np.matrix(np.zeros(t.shape[1]))
+                for i in np.random.randint(0,len(t),batch_size):
+                    delta += (alpha * ((y[i] - t[i] * w) * t[i]))
+                if np.all(np.abs(delta) < 0.001) or np.any(np.isnan(delta)):
                     break
                 w += delta.reshape(w.shape)
+
             self._w = w
-            return w
+        return self._w
 
     def predict(self,x):
         t = np.matrix(np.append(x,np.ones((len(x),1)),axis=1))
--- tests/test_features.py
+++ tests/test_features.py
@@ -4,9 +4,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    x,y = generate_linear_classification_data(n1=50,n2=50)
-    print polynomial(x)
 
+    train_x,train_y = generate_linear_regression_data(n=100,f = lambda x : x**2 + 2*x + 2)
+    regression = LinearRegression(ne=False)
+    poly_train_x = polynomial(np.append(train_x,np.ones((len(train_x),1)),axis=1))[:,:2]
+    w = regression.train(poly_train_x,train_y,alpha=0.000001,batch_size=len(poly_train_x))
+
+    test_x = np.linspace(-10,10,100).reshape(100,1)
+    poly_test_x = polynomial(np.append(test_x,np.ones((len(test_x),1)),axis=1))[:,:2]
+    test_y = regression.predict(poly_test_x)
+
+    plt.plot(train_x,train_y,'ro')
+    plt.plot(test_x,test_y)
+    plt.show()
+
+    # x,y = generate_linear_classification_data(n1=50,n2=50)
     # compressed_x,vec = pca_compress(x)
     # res = pca_decompress(compressed_x,vec)
     #
